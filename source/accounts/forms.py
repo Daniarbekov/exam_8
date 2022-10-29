91f7e36b0ed25c02d7f30564bef6f17353f8ce2c
Daniarbekov <daniarbekov@gmail.com>
2022-10-29 17:30:48 +0600

    Создано вью регистрации
    создан шаблон страницы
    добавлена ссылка
    создана форма

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -15,3 +15,20 @@ class LoginForm(forms.Form):
         user = get_user_model().objects.get(username=username)
         if user and not user.check_password(password):
             raise forms.ValidationError('Неверный пароль!')
+
+
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='повторите пароль', widget=forms.PasswordInput)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'first_name', 'last_name','email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('Пароли не совпадают')
+        return cd['password2']
